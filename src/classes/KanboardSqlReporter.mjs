b314e7031059addbbd61a5b3615104b34dda29ea
gzb <gaspardzebear@hotmail.fr>
2026-02-27 11:41:18 +0100

    select users

--- src/classes/KanboardSqlReporter.mjs
+++ src/classes/KanboardSqlReporter.mjs
@@ -5,9 +5,9 @@ import { DatabaseSync } from 'node:sqlite';
 
 class KanboardSqlReporter {
 
-
+  // PCST mean Projet, Swimlane, Column, Task !!!!!
   constructor(dbFile, runOnCreate) {
-    console.log("KanboardSqlReporter.constructor() ",dbFile, "runOnCreate ",runOnCreate)
+    console.log("KanboardSqlReporter.constructor() ", dbFile, "runOnCreate ", runOnCreate)
     this.db = new DatabaseSync('db.sqlite', { readonly: true });
     if (runOnCreate) {
       this.run()
@@ -20,24 +20,24 @@ class KanboardSqlReporter {
   }
 
   //-----------------------------------------------------
-  async getJsonReport() {
-    console.log("KanboardSqlReporter.getJsonReport() req0")
-    let req0 = `
+  async selectPCST() {
+    console.log("KanboardSqlReporter.selectPCST()")
+    let reqPCST = `
       select 
-            p.id pId,
-	    p.name pName,
-      p.description pDescription,
-	    s.id sId,
-	    s.name sName,
- 	    c.id cId,
-	    c.title cTitle,
-      t.id tId,
-	    t.title tTitle,
-      t.owner_id tOwnerId,
-      t.color_id tColor,
-	    t.date_moved tMoved,
-	    t.date_due tDue,
-	    datetime(t.date_moved,'unixepoch') tMovedDatetime
+        p.id pId,
+	      p.name pName,
+        p.description pDescription,
+	      s.id sId,
+	      s.name sName,
+ 	      c.id cId,
+	      c.title cTitle,
+        t.id tId,
+	      t.title tTitle,
+        t.owner_id tOwnerId,
+        t.color_id tColor,
+	      t.date_moved tMoved,
+	      t.date_due tDue,
+	      datetime(t.date_moved,'unixepoch') tMovedDatetime
       from tasks as t
       join swimlanes as s
         on s.id=t.swimlane_id
@@ -47,11 +47,33 @@ class KanboardSqlReporter {
         on c.id=t.column_id
       order by p.name,s.name,t.title
       `
-    const queryStmt = this.db.prepare(req0);
+    const queryStmt = this.db.prepare(reqPCST);
+    return(queryStmt.all())
     //console.log(queryStmt.all())
+  }
+
+  //-----------------------------------------------------
+  async selectUsers() {
+    console.log("KanboardSqlReporter.selectPCST()")
+    let reqUsers = `select u.id uId,u.username uUsername, u.name uName from users as u`
+    let usersMap = { '0': 'nobody' }
+    const queryStmt = this.db.prepare(reqUsers);
+    for (let row of queryStmt.all()) {
+      usersMap[row.uId] = { name: row.uName, username: row.uUsername }
+    }
+    return(usersMap)
+  }
+
+  //-----------------------------------------------------
+  async getJsonReport() {
     let report = []
     let projectsMap = {}
-    for (let row of queryStmt.all()) {
+    const pcstPromises = this.selectPCST()
+    const usersPromise = this.selectUsers()
+    let [pcst,usersMap]=await Promise.all([pcstPromises,usersPromise])
+
+    //-- turn into table
+    for (let row of pcst) {
       console.log(row)
       if (!projectsMap[row.pId]) {
         projectsMap[row.pId] = {}
@@ -60,7 +82,7 @@ class KanboardSqlReporter {
         projectsMap[row.pId].swimlanes = {}
         projectsMap[row.pId].columns = {}
         projectsMap[row.pId].tags = {}
-        projectsMap[row.pId].users = {}
+        projectsMap[row.pId].users = usersMap
       }
       if (!projectsMap[row.pId].columns[row.cId]) {
         projectsMap[row.pId].columns[row.cId] = { id: row.cId, title: row.cTitle }
@@ -81,7 +103,8 @@ class KanboardSqlReporter {
       report.push(projectsMap[p])
     }
     console.log(report)
-    return(report)
+    console.log(usersMap)
+    return (report)
     //const columns = queryStmt.columns();
     //for (let column of columns) {
     //console.log(column)
