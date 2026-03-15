2d052ec15166fdc62df3cc3a7c7f0b6db4bee649
gzb <gaspardzebear@hotmail.fr>
2026-03-15 10:54:45 +0100

    route/swimlanes

--- src/apigateway/routes/swimlanes.mjs
+++ src/apigateway/routes/swimlanes.mjs
@@ -1,13 +1,26 @@
 import express from 'express'
 const router = express.Router();
 import { swimlaneController } from '../controllers/swimlanes.mjs'
+import { UnifiedController } from '../controllers/UnifiedController.mjs';
 
-router.post('/', swimlaneController.createSwimlane);
-router.get('/', swimlaneController.getAllSwimlanes);
-router.get('/:id', swimlaneController.getSwimlaneById);
+//router.post('/', swimlaneController.createSwimlane);
+router.post('/', UnifiedController.getFunction('swimlanes','create'))
+//router.get('/', swimlaneController.getAllSwimlanes);
+router.get('/', UnifiedController.getFunction('swimlanes','getAll'))
+//router.get('/:id', swimlaneController.getSwimlaneById);
+router.get('/', UnifiedController.getFunction('swimlanes','getById'))
+
+
+// To be seen !!!!!!!!!!!!!!!!!!!!!
 router.get('/project/:projectId', swimlaneController.getSwimlanesByProjectId);
-router.put('/:id', swimlaneController.updateSwimlane);
-router.delete('/:id', swimlaneController.deleteSwimlane);
+
+//router.put('/:id', swimlaneController.updateSwimlane);
+router.put('/:id', UnifiedController.getFunction('swimlanes','update'));
+
+// Did not exist
+router.patch('/:id', UnifiedController.getFunction('swimlanes','patch'));
+//router.delete('/:id', swimlaneController.deleteSwimlane);
+router.delete('/:id', UnifiedController.getFunction('swimlanes','delete'));
 export default router
 
 
