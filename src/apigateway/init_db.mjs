e9d2151d203973f36eb69b4faf9ad8316c6b331f
gzb <gaspardzebear@hotmail.fr>
2026-03-26 10:18:42 +0100

    mainly constatrints

--- src/apigateway/init_db.mjs
+++ src/apigateway/init_db.mjs
@@ -14,10 +14,10 @@ import {db } from './config/database.mjs'
       id INTEGER PRIMARY KEY,
       name TEXT NOT NULL,
       position INTEGER,
-      project_id INTEGER NOT NULL,
+      project_id INTEGER NOT NULL DEFAULT 0,
       color TEXT DEFAULT 'white',
       description TEXT,
-      FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET NULL,
+      FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET DEFAULT,
       UNIQUE (name, project_id)
     )
   `);
@@ -27,20 +27,20 @@ import {db } from './config/database.mjs'
       id INTEGER PRIMARY KEY,
       name TEXT NOCASE NOT NULL,
       description TEXT,
-      workspace_id INTEGER NOT NULL,
+      workspace_id INTEGER NOT NULL DEFAULT 0,
       is_open INTEGER DEFAULT 1,
-      FOREIGN KEY(workspace_id) REFERENCES workspaces(id) ON DELETE SET  NULL,
+      FOREIGN KEY(workspace_id) REFERENCES workspaces(id) ON DELETE SET DEFAULT,
       UNIQUE (name, id)
     )
   `);
 
   db.exec(`
     CREATE TABLE IF NOT EXISTS projects_users (
-      project_id INTEGER NOT NULL,
-      user_id INTEGER NOT NULL,
+      project_id INTEGER NOT NULL DEFAULT 0,
+      user_id INTEGER NOT NULL DEFAULT 0,
       right INTEGER NOT NULL DEFAULT 0,
-      FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET NULL,
-      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE SET NULL,
+      FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET DEFAULT,
+      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE SET DEFAULT,
       UNIQUE(project_id, user_id)
     )
   `);
@@ -51,9 +51,9 @@ import {db } from './config/database.mjs'
       name TEXT NOT NULL,
       position INTEGER DEFAULT 1,
       is_open INTEGER DEFAULT 1,
-      project_id INTEGER NOT NULL,
+      project_id INTEGER NOT NULL DEFAULT 0,
       description TEXT,
-      FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET NULL,
+      FOREIGN KEY(project_id) REFERENCES projects(id) ON DELETE SET DEFAULT,
       UNIQUE (name, project_id)
     )
   `);
@@ -63,9 +63,9 @@ import {db } from './config/database.mjs'
       id INTEGER PRIMARY KEY,
       name TEXT NOT NULL,
       description TEXT,
-      project_id INTEGER NOT NULL,
+      workspace_id INTEGER NOT NULL,
       color TEXT DEFAULT 'yellow',
-      UNIQUE(project_id, name)
+      UNIQUE(workspace_id, name)
     )
   `);
 
@@ -79,13 +79,13 @@ import {db } from './config/database.mjs'
       note TEXT,
       color TEXT,
       color_rules TEXT,
-      column_id INTEGER NOT NULL REFERENCES columns(id) ON DELETE  SET NULL,
-      creator_id INTEGER REFERENCES users(id) ON DELETE  SET NULL,
-      assignee_id INTEGER REFERENCES assignees(id) ON DELETE  SET NULL,
+      column_id INTEGER NOT NULL DEFAULT 0,
+      user_id INTEGER DEFAULT 0,
+      assignee_id INTEGER DEFAULT 0,
       position INTEGER,
       is_open INTEGER DEFAULT 1,
       priority INTEGER DEFAULT 0,
-      swimlane_id INTEGER NOT NULL REFERENCES swimlanes(id) ON DELETE  SET NULL,
+      swimlane_id INTEGER NOT NULL DEFAULT 0,
       date_created DATETIME DEFAULT CURRENT_TIMESTAMP,
       date_started INTEGER DEFAULT 0,
       date_moved INTEGER DEFAULT 0,
@@ -96,29 +96,32 @@ import {db } from './config/database.mjs'
       moved_warning TEXT,
       due_warning TEXT,
       checked_warning TEXT,
-      UNIQUE(name,swimlane_id,column_id)
+      UNIQUE(name,swimlane_id,column_id),
+      FOREIGN KEY(swimlane_id) REFERENCES swimlanes(id) ON DELETE SET DEFAULT,
+      FOREIGN KEY(column_id) REFERENCES columns(id) ON DELETE SET DEFAULT
     )
   `);
 
   db.exec(`
     CREATE TABLE IF NOT EXISTS tasks_comments (
       id INTEGER PRIMARY KEY,
-      task_id INTEGER NOT NULL,
-      user_id INTEGER DEFAULT 0,
+      task_id INTEGER NOT NULL DEFAULT 0,
+      user_id INTEGER NOT NULL DEFAULT 0,
       date_created INTEGER NOT NULL,
       comment TEXT NOT NULL,
       reference VARCHAR(50),
       date_modified INTEGER,
-      FOREIGN KEY(task_id) REFERENCES tasks(id) ON DELETE SET  NULL
+      FOREIGN KEY(task_id) REFERENCES tasks(id) ON DELETE SET DEFAULT,
+      FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE SET DEFAULT
     )
   `);
 
   db.exec(`
     CREATE TABLE IF NOT EXISTS task_has_tags (
-      task_id INTEGER NOT NULL,
-      tag_id INTEGER NOT NULL,
-      FOREIGN KEY(task_id) REFERENCES tasks(id) ON DELETE  SET NULL,
-      FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE  SET NULL
+      task_id INTEGER NOT NULL DEFAULT 0,
+      tag_id INTEGER NOT NULL DEFAULT 0,
+      FOREIGN KEY(task_id) REFERENCES tasks(id) ON DELETE SET DEFAULT,
+      FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE SET DEFAULT
     )
   `);
 
